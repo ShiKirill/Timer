509d2f905e9f5eb3cf3138c8a752674adde00bd0
ShiKirill <shiraevkirill@gmail.com>
2021-03-02 18:59:11 +0700

    Lessin 24

--- js/script.js
+++ js/script.js
@@ -316,15 +316,53 @@ window.addEventListener('DOMContentLoaded', () => {
 
     photoChange();
 
-    const calculator = () => {
+    const calculator = (price = 100) => {
         const calcBlock = document.querySelector('.calc-block');
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcDay = document.querySelector('.calc-day');
+        const calcCount = document.querySelector('.calc-count');
+        const totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0;
+            let countValue = 1;
+            let dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+
+            let squareValue = +calcSquare.value;
+
+            if (calcCount.value> 1) {
+                countValue += (calcCount.value-1)/10;
+            }
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10){
+                dayValue *=1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        }
+
         calcBlock.addEventListener('input', event => {
             const target = event.target;
-            target.value = target.value.replace(/\D/, '');
+            if (target.matches('input')) {
+                target.value = target.value.replace(/\D/, '');
+            }
+        });
+        calcBlock.addEventListener('change', event => {
+            let target = event.target;
+                if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
         });
     }
 
-    calculator();
+    calculator(100);
 
     const questionForm = () => {
         const inputForm = document.querySelector('.footer-form-input');
